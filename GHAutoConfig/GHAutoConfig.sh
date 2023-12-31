54603aa997d9bfbdd938f9ad0dd0ec61bc503b32
YoRHa nya <108107785+nya-main@users.noreply.github.com>
2023-12-31 10:35:35 +0800

    Update GHAutoConfig.sh

--- GHAutoConfig/GHAutoConfig.sh
+++ GHAutoConfig/GHAutoConfig.sh
@@ -6,9 +6,9 @@ set -x
 #配置默认初始仓库分支为main
 git config --global init.defaultBranch "main"
 #配置username(可改为你自己的GitHub用户名)
-git config --global user.name "nya-sec"
+git config --global user.name "Rhine Nya"
 #配置用户邮箱(可改为你自己注册GitHub邮箱)
-git config --global user.email "nyaisacat@gmail.com"
+git config --global user.email "108107785+nya-main@users.noreply.github.com"
 #配置最大缓冲区大小为10G(防止推送/拉取失败)
 git config --global http.postBuffer 10737418240
 git config --global https.postBuffer 10737418240
