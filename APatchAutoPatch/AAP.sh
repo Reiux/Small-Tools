91bedd4bf273e5e412e5d9d42e46bb48043580dd
Rhine Nya <108107785+nya-main@users.noreply.github.com>
2024-02-04 23:15:30 +0800

    完成脚本

--- APatchAutoPatch/AAP.sh
+++ APatchAutoPatch/AAP.sh
@@ -12,6 +12,7 @@ YELLOW="\E[1;33m"
 RESET="\E[0m"
 
 BOOTSUFFIX=$(getprop ro.boot.slot_suffix)
+SUPERKEY=${RANDOM}
 WORKDIR=/data/adb/nyatmp
 
 mkdir -p ${WORKDIR}
@@ -39,6 +40,7 @@ get_tools() {
 		echo "Please check your internet connection."
 		exit 1
 	fi
+	chmod +x kptools-android
 	echo "I: Done."
 	echo "I: Downloading the latest kpimg-android..."
 	curl -LO "https://github.com/bmax121/KernelPatch/releases/latest/download/kpimg-android"
@@ -49,12 +51,48 @@ get_tools() {
 		exit 1
 	fi
 	echo "I: Done."
+	echo "I: Downloading magiskboot..."
+	curl -LO "https://github.com/magojohnji/magiskboot-linux/raw/main/arm64-v8a/magiskboot"
+	EXITSTATUS=$?
+	if [[ $EXITSTATUS != 0 ]]; then
+		echo -e "${RED}E: DOWNLOAD FAILED${RESET}"
+		echo "Please check your internet connection."
+		exit 1
+	fi
+	chmod +x magiskboot
+	echo "I: Done."
 }
 
-patch_boot() {}
+patch_boot() {
+	echo "I: Unpacking image..."
+	./magiskboot unpack boot${BOOTSUFFIX}.img
+	EXITSTATUS=$?
+	if [[ $EXITSTATUS != 0 ]]; then
+		echo -e "${RED}E: UNPACK BOOT IMAGE FAILED${RESET}"
+		exit 1
+	fi
+	echo "I: Done."
+	echo "I: Patching image...Current Superkey: ${SUPERKEY}"
+	./kptools-android -p -k kpimg-android -s ${SUPERKEY} -i kernel -o patchedkernel
+	EXITSTATUS=$?
+	if [[ ${EXITSTATUS} != 0 ]]; then
+		echo -e "${RED}E: PATCH FAILED${RESET}"
+		exit 1
+	fi
+	echo "I: Done"
+	echo "I: Repacking..."
+	rm kernel
+	mv patchedkernel kernel || EXITSTATUS=1
+	magiskboot repack boot${BOOTSUFFIX} patched_boot || EXITSTATUS=1
+	if [[ $EXITSTATUS != 0 ]]; then
+		echo -e "${RED}E: REPACK FAILED${RESET}"
+		exit 1
+	fi
+	echo "I: Done."
+}
 
 flash_boot() {
-	dd if=${WORKDIR}/patched_boot${BOOTSUFFIX}.img of=/dev/block/by-name/boot${BOOTSUFFIX}
+	dd if=${WORKDIR}/patched_boot.img of=/dev/block/by-name/boot${BOOTSUFFIX}
 	EXITSTATUS=$?
 	if [[ ${EXITSTATUS} != 0 ]]; then
 		echo -e "${RED}E: WARNING!!! IMAGE FLASH FAILED${RESET}"
@@ -70,3 +108,8 @@ flash_boot() {
 	fi
 	echo "I: Flash done."
 }
+
+get_boot
+get_tools
+patch_boot
+flash_boot
